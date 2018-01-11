8af7051313aa0bc183d09a27af0e334c40f2714b
zen4orce-Ajinkya <ajinkya.kasabe@zen4orce.com>
2018-01-11 15:31:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ const bodyParser = require("body-parser");
 
 
 let SalesforceConnection = require("node-salesforce-connection");
+
+(async () => {
 let sfConn = new SalesforceConnection();
-/*
+
 await sfConn.soapLogin({
     hostname: "login.salesforce.com",
     apiVersion: "39.0",
     username: "ajinkya33@zen4orce.com",
     password: "Ajinkya@33ymtTsmynVY7EUOcZJeXlU2VV",
   });
-
+/*
 let recentAccounts = await sfConn.rest("/services/data/v39.0/query/?q="
   + encodeURIComponent("select Id, Name from Account where CreatedDate = LAST_WEEK"));
 
 for (let account of recentAccounts.records) {
   console.log("Account " + account.Name + " was created recently.");
   }*/
-
+})().catch(ex => console.error(ex.stack));
 const restService = express();
 
 restService.use(
